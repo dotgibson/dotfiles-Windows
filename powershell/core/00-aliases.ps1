f60a9cfddff9069ad317c94b18748627aabe4ab3
Gerrrt <garrettallen2@gmail.com>
2026-06-05 15:55:58 -0700

    updates

--- powershell/core/00-aliases.ps1
+++ powershell/core/00-aliases.ps1
@@ -73,3 +73,6 @@ function mkcd  { param($p) New-Item -ItemType Directory -Force -Path $p | Out-Nu
 function which { param($n) (Get-Command $n -ErrorAction SilentlyContinue).Source }
 function reload { . $PROFILE; Write-Host 'profile reloaded' -ForegroundColor Green }
 function dotfiles { Set-Location $global:DOTFILES }
+
+# --- Neovim -------------------------------------------------------------------
+Set-Alias vim nvim
